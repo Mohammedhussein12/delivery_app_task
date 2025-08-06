a32e883d5810f6fbbb9d0fe86d7befd9faf222df
mohammed hussein <muhammedhussein739@gmail.com>
2025-08-06 15:07:56 +0300

    - added app_colors - added app_assets - added app_styles - added flutter_screen_util for responsiveness

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff5117AC);
+  static const Color cyanColor = Color(0xff20D0C4);
+  static const Color unSelectedItemColor = Color(0xffBBBBBB);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color lightGreyColor = Color(0xff9A9A9A);
+  static const Color darkGreyColor = Color(0xff515F65);
+  static const Color redColor = Color(0xffF1395E);
+  static const Color softIceBlueColor = Color(0xFFE2EDF2);
+  static const Color deepRoyalBlueColor = Color(0xFF153E73);
+}
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,103 @@
+import 'dart:ui';
+
+import 'package:delivery_app_task/core/utils/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular10 = TextStyle(
+    color: AppColors.softIceBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleSemiBold22 = TextStyle(
+    color: AppColors.primaryColor,
+    fontFamily: 'Poppins',
+    fontSize: 22.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleSemiBold16 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleRegular12 = TextStyle(
+    color: AppColors.lightGreyColor,
+    fontFamily: 'Poppins',
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleMedium12 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleLight7 = TextStyle(
+    color: AppColors.darkGreyColor,
+    fontFamily: 'Poppins',
+    fontSize: 7.sp,
+    fontWeight: FontWeight.w100,
+  );
+  static TextStyle styleSemiBold14 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleLight10 = TextStyle(
+    color: AppColors.lightGreyColor,
+    fontFamily: 'Poppins',
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w100,
+  );
+  static TextStyle styleSemiBold18 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleLight11 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w100,
+  );
+  static TextStyle styleSemiBold30 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleMedium18 = TextStyle(
+    color: AppColors.whiteColor,
+    fontFamily: 'Poppins',
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleSemiBold10 = TextStyle(
+    color: AppColors.whiteColor,
+    fontFamily: 'Poppins',
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleRegular9 = TextStyle(
+    color: AppColors.whiteColor,
+    fontFamily: 'Poppins',
+    fontSize: 9.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleSemiBold19 = TextStyle(
+    color: AppColors.deepRoyalBlueColor,
+    fontFamily: 'Poppins',
+    fontSize: 19.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleSemiBold20 = TextStyle(
+    color: AppColors.cyanColor,
+    fontFamily: 'Poppins',
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+  );
+}
